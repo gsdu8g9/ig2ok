7ba6568d91b678f7a3c8cf43f2bcf7baff50e459
Anton Korenskoy <anton@korenskoy.ru>
2014-12-24 17:00:03 +1000

    Первая итерация, которая хоть как-то работает

---
+++ ok.js
@@ -0,0 +1,135 @@
+var config = require('config').config;
+var casper = require('casper').create({
+    verbose:      true,
+    logLevel:     "info",
+    waitTimeout:  20000,
+    viewportSize: {width: 480, height: 800},
+    pageSettings: {
+        userAgent:   config.user_agent,
+        loadImages:  false,        // The WebPage instance used by Casper will
+        loadPlugins: false         // use these settings
+    }
+});
+
+var photos = [];
+
+casper.on('remote.message', function (message) {
+    this.echo('Page Debug Data:       ' + message);
+});
+
+casper.on('remote.alert', function (message) {
+    this.echo('alert message: ' + message);
+    // or if you want to test it
+
+    this.captureSelector('alert_' + (Math.random()) + '.jpg', '#schedule', {
+        format:  'jpg',
+        quality: 75
+    });
+});
+
+// Авторизация
+casper.start(config.url, function () {
+     this.fillSelectors(
+         '#content form',
+         {
+             '#field_login':    config.user,
+             '#field_password': config.pass
+         }, true);
+});
+
+casper.thenOpen('http://instagram.com/old_vl', function () {
+    var js = this.evaluate(function () {
+        return document;
+    });
+    photos = js.all[0].outerHTML.match(/instagram.com\/p\/([\w\d]+)/igm);
+});
+
+casper.then(function () {
+    debugger;
+    photos = photos.slice(0,3);
+    casper.each(photos, function (self, url) {
+        casper.echo("Getting entries for keyword " + url + "..");
+
+        casper.thenOpen('http://' + url, function () {
+            debugger;
+            var js = this.evaluate(function () {
+                return document;
+            });
+            photos = js.all[0].outerHTML;
+
+            function fff(cssSelector) {
+                return document.querySelectorAll(cssSelector);
+            }
+
+            var data = this.evaluate(fff, '.photo.Frame.Image');
+            var src = '';
+            for (var i = 0; i < data[0].attributes.length; i++) {
+                if (data[0].attributes[i].name === 'src') {
+                    src = data[0].attributes[i].value;
+                    break;
+                }
+            }
+            var text = this.evaluate(fff, '.caption-text');
+            this.echo(src);
+            try {
+                text = text[0].innerHTML;
+            } catch  (e) {
+                text = 'From http://instagram.com/old_vl';
+            }
+
+            if (text.length > 0 && src.length > 0) {
+                var filename = src.match(/([-\._\w]+)$/gi);
+                casper.then(function () {
+                    this.download(src, 'downloads/' + filename[0]);
+                });
+
+                casper.thenOpen(config.albom_url, function () {
+                    this.echo(this.getTitle());
+                    this.captureSelector('yoursitelist2.png', 'body');
+                });
+
+                casper.then(function () {
+                    this.click('#group-album a.add-pho-plch');
+                });
+
+                casper.then(function () {
+                    debugger;
+                    this.fillSelectors(
+                        'form.js-sm-upload-form',
+                        {
+                            '#field_file': 'downloads/' + filename[0]
+                        }, true);
+
+                    this.captureSelector('yoursitelist3.png', 'body');
+                });
+
+                casper.then(function () {
+                    this.waitForSelector('#content form', function () {
+                        debugger
+                        console.log(text.replace(/\s@([-_\.\w\d]+)/gi, ' http://instagram.com/$1').substr(0, 255));
+                        this.fillSelectors(
+                            '#content form',
+                            {
+                                '#field_msg': text.replace(/\s@([-_\.\w\d]+)/gi, ' http://instagram.com/$1').substr(0, 255)
+                            }, true
+                        );
+                        this.captureSelector('yoursitelist4.png', 'body');
+                    });
+                });
+
+                casper.then(function () {
+                    this.click('[name=button_save]');
+
+                    this.captureSelector('yoursitelist5.png', 'body');
+                });
+            }
+        });
+    })
+});
+
+
+
+casper.run(function () {
+    // echo results in some pretty fashion
+    this.echo('Done').exit();
+});
